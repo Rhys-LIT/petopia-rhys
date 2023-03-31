428e70f00a4b82c426d1ce67b6db547d044d1c97
RhysQuilter <98286839+Rhys-LIT@users.noreply.github.com>
2023-03-31 12:13:04 +0100

    OrderService

---
+++ src/main/java/com/example/assignmenttwo_starter/services/OrderService.java
@@ -0,0 +1,43 @@
+package com.example.assignmenttwo_starter.services;
+
+import com.example.assignmenttwo_starter.model.Order;
+import com.example.assignmenttwo_starter.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public Order createOrder(Order order) {
+        return orderRepository.save(order);
+    }
+    public Order deleteOrder(Integer orderId) {
+    Order order = orderRepository
+            .findById(orderId)
+            .orElseThrow(() -> new IllegalArgumentException("Order with ID " + orderId + " not found"));
+
+    orderRepository.delete(order);
+    return order;
+}
+    public List<Order> findAll() {
+        return orderRepository.findAll();
+    }
+    public Page<Order> findAll(PageRequest pageRequest) {
+        return orderRepository.findAll(pageRequest);
+    }
+    public Optional<Order> findById(Integer orderId) {
+        return orderRepository.findById(orderId);
+    }
+    public Order updateOrder(Order order) {
+        return orderRepository.save(order);
+    }
+}
+
+
